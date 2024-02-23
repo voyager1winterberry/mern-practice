2ee7e32182ca809d8755d589d3aca810c3ed0d66
Marcus <voyager1winterberry@gmail.com>
2024-02-22 18:24:59 -0800

    removed comments

--- pro-mern-stack-2/project/src/App.jsx
+++ pro-mern-stack-2/project/src/App.jsx
@@ -1,9 +1,3 @@
-/*
-the transformation of JSX to JavaScript happened at runtime with past
-code. this is inefficient. here, the transformation is moved to the 
-build state in our development so that a ready-to-use version 
-can be deployed.
-*/
 const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
 const helloContinents = Array.from(continents, c => `Hello, ${c}!`);
 const message = helloContinents.join(' ');
